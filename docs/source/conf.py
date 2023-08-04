87c4b134de210c905c3c4a5d13b37818bf3cb763
Aleksi Ikkala <aleksi.ikkala@gmail.com>
2023-08-04 17:04:18 +0300

    Bump version, enable autopi, add requirement versions

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -16,8 +16,8 @@ project = 'MyoConverter'
 copyright = "Aleksi Ikkala, Huawei Wang"
 author = "Aleksi Ikkala, Huawei Wang"
 
-release = '0.1'
-version = '0.1.0'
+release = '1.0'
+version = '1.0.0'
 
 # -- General configuration
 
@@ -25,11 +25,11 @@ extensions = [
     'sphinx.ext.duration',
     'sphinx.ext.doctest',
     'sphinx.ext.intersphinx',
-    'sphinx.ext.viewcode'
-    # 'autoapi.extension'
+    'sphinx.ext.viewcode',
+    'autoapi.extension',
 ]
-# autoapi_type = 'python'
-# autoapi_dirs = ["../../myoconverter"]
+autoapi_type = 'python'
+autoapi_dirs = ["../../myoconverter"]
 
 intersphinx_mapping = {
     'python': ('https://docs.python.org/3/', None),
