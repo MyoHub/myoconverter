ca80a4dcde337eb5b535ba1c9db2340ccfc02e3a
ml <huawei.wang.buaa@gmail.com>
2023-08-05 13:05:32 +0200

    break down example codes to each MSK model

---
+++ examples/arm26.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+This script includes all the steps of transfering an OpenSim model to Mujoco.
+
+Created on Tue Jul 27 23:19:13 2021
+
+@author: hwang
+
+"""
+from myoconverter.O2MPipeline import O2MPipeline
+
+# define pipline configurations
+kwargs = {}  # define kwargs inputs
+kwargs['convert_steps'] = [1, 2, 3]    # All three steps selected
+kwargs['muscle_list'] = None           # No specific muscle selected, optimize all of them
+kwargs['osim_data_overwrite'] = True   # Overwrite the Osim model state files
+kwargs['conversion'] = True            # Yes, perform 'Cvt#' process
+kwargs['validation'] = True            # Yes, perform 'Vlt#' process
+kwargs['speedy'] = True                # Do not reduce the checking notes to increase speed
+kwargs['generate_pdf'] = True          # Do not generate validation pdf report
+kwargs['add_ground_geom'] = True       # Add ground to the model
+kwargs['treat_as_normal_path_point'] = False    # Using constraints to represent moving and conditional path points
+
+
+############### Simple Arm 2 DoFs 6 Muscles ################ 
+osim_file = './models/osim/Arm26/arm26.osim'
+geometry_folder = './models/osim/Arm26/Geometry'
+output_folder = './models/mjc/Arm26'
+O2MPipeline(osim_file, geometry_folder, output_folder, **kwargs)
